5e393f76d1e51a3b27a80d5b719b8dc18b564884
VicentBG <vbatallerg@gmail.com>
2021-01-01 21:50:39 +0100

    Inicio de la evaluable, add archivos base

---
+++ Evaluacion2_Parte2/actores_form.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Edición de actores</title>
+</head>
+
+<body>
+    <div class="alert alert-secondary d-flex">
+        <a href="./peliculas.php" class="btn btn-dark">Películas</a>&nbsp;&nbsp;
+    </div>
+    <div class="container">
+        <!-- ESCRIBE AQUÍ TU CODIGO -->
+    </div>
+
+</body>
+
+</html>
---
+++ Evaluacion2_Parte2/directores_ficha.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Directores | Ficha</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/estilos.css">
+</head>
+
+<body>
+    <div class="alert alert-secondary d-flex">
+        <a href="./peliculas.php" class="btn btn-dark">Películas</a>&nbsp;&nbsp;
+    </div>
+    <div class="container">
+        <!-- ESCRIBE AQUÍ TU CÓDIGO -->
+    </div>
+</body>
+
+</html>

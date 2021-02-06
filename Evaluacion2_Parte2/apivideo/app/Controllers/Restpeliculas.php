7daadfa88a9eea06fada0fb151ad6b5640eff059
VicentBG <vbatallerg@gmail.com>
2021-02-06 11:57:19 +0100

    Creacion rest y validacion peliculas

---
+++ Evaluacion2_Parte2/apivideo/app/Controllers/Restpeliculas.php
@@ -0,0 +1,150 @@
+<?php
+namespace App\Controllers;
+use CodeIgniter\RESTful\ResourceController;
+use App\Models\PeliculaModel;
+
+class Restpeliculas extends ResourceController
+{
+     protected $modelName = 'App\Models\PeliculaModel';
+     protected $format = 'json';
+
+    /**
+     * Función que devuelve el estado de la petición, si error muestra mensaje
+     */
+    private function genericResponse($data, $msj, $code)
+    {
+
+        if ($code == 200) {
+            return $this->respond(
+                array(
+                "data" => $data,
+                "code" => $code
+                )
+            );
+        } else {
+            return $this->respond(
+                array(
+                "msj" => $msj,
+                "code" => $code
+                )
+            );
+        }
+    }
+
+    /**
+     * Función donde obtenemos el base_url
+     */
+    private function url($segmento)
+    {
+        if (isset($_SERVER['HTTPS'])) {
+            $protocol = ($_SERVER['HTTPS'] && $_SERVER['HTTPS'] != "off") ? "https" : "http";
+        } else {
+            $protocol = 'http';
+        }
+        return $protocol . "://" . $_SERVER['HTTP_HOST'] . $segmento;
+    }
+
+    /**
+     * Función que construye los datos
+     */
+    private function map($data)
+    {
+        $peliculas = array();
+        foreach ($data as $row) {
+            $pelicula = array(
+                "id" => $row['id'],
+                "titulo" => $row['titulo'],
+                "anyo" => $row['anyo'],
+                "duracion" => $row['duracion'],
+                "links" => array(
+                    array("rel" => "self","href" => $this->url("/restpeliculas/".$row['id']),"action" => "GET", "types" =>["text/xml","application/json"]),
+                    array("rel" => "self","href" => $this->url("/restpeliculas/".$row['id']), "action"=>"PUT", "types" => ["application/x-www-form-urlencoded"]),
+                    array("rel" => "self","href" => $this->url("/restpeliculas/".$row['id']), "action"=>"PATCH" ,"types" => ["application/x-www-form-urlencoded"]),
+                    array("rel" => "self","href" => $this->url("/restpeliculas/".$row['id']), "action"=>"DELETE", "types"=> [] )
+                )
+               
+            );
+            array_push($peliculas, $pelicula);
+        }
+        return $peliculas;
+    }
+
+    // OPERACIONES TIPO GET: devuelve todas o una película (por id)
+    public function index()
+    {
+        $data = $this->model->getAll();
+        $peliculas = $this->map($data);
+       
+        return $this->genericResponse($peliculas, null, 200);
+    }
+ 
+    public function show($id = null)
+    {
+        $data = $this->model->get($id);      
+        $pelicula = $this->map($data); 
+
+        return $this->genericResponse($pelicula, null, 200);
+    }
+
+    // OPERACIONES TIPO POST: crea una nueva película
+    public function create()
+    {
+        $peliculas = new PeliculaModel();
+ 
+        if ($this->validate('peliculas')) {
+            $id = $peliculas->insert(
+                [
+                'titulo' => $this->request->getPost('titulo'),
+                'anyo' => $this->request->getPost('anyo'),
+                'duracion' => $this->request->getPost('duracion')
+                ]
+            );
+
+            return $this->genericResponse($this->model->find($id), null, 200);
+        }
+ 
+        //Hemos creado validaciones en el archivo de configuración Validation.php
+        $validation = \Config\Services::validation();
+        //Si no pasa la validación devolvemos un error 500
+        return $this->genericResponse(null, $validation->getErrors(), 500);
+    }
+
+
+    // OPERACIONES TIPO PUT O PATCH: actualización de un recurso
+    public function update($id = null)
+    {
+        $peliculas = new PeliculaModel();
+
+        // Al ser un método de tipo PUT o PATCH debemos recoger los datos usando el método getRawInput
+        $data = $this->request->getRawInput();
+ 
+        if ($this->validate('peliculas')) {
+ 
+            if (!$peliculas->get($id)) {
+                return $this->genericResponse(null, array("id" => "La película no existe"), 500);
+            }
+ 
+            $peliculas->update(
+                $id,
+                [
+                "id" => $row['id'],
+                "titulo" => $row['titulo'],
+                "anyo" => $row['anyo'],
+                "duracion" => $row['duracion']
+                ]
+            );
+ 
+            return $this->genericResponse($this->model->find($id), null, 200);
+        }
+
+    }
+
+    // OPERACIONES DELETE: borrado de un recurso
+    public function delete($id = null)
+    {
+        $peliculas = new PeliculaModel();
+        $peliculas->delete($id);
+ 
+        return $this->genericResponse("Película borrada", null, 200);
+    }
+}
